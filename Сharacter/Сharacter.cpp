2fe37fda5c3c2e179b4861eda06af804bd778e08
FacePunch1337 <rezol1337@gmail.com>
2021-11-13 19:52:31 +0300

    Добавьте файлы проекта.

---
+++ Сharacter/Сharacter.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+
+class OrcWarrior
+{
+public:
+    OrcWarrior() {
+        std::cout << "Constructor OrcWarrior" << std::endl;
+    }
+
+    class Orc
+    {
+    public:
+         Orc() {
+                std::cout << "Constructor Orc" << std::endl;
+          }
+    class Character
+    {
+    public:
+        Character() {
+            std::cout << "Constructor Character" << std::endl;
+        }
+    };
+    };
+
+    class Warrior
+    {
+    public:
+        Warrior() {
+            std::cout << "Constructor Warrior" << std::endl;
+         }
+         class Character
+         {
+          public:
+            Character() {
+            std::cout << "Constructor Character" << std::endl;
+             }
+    };
+};
+
+
+};
+
+
+int main()
+{
+   OrcWarrior::Orc::Character character;
+}
+
